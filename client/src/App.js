f325ed97c5777857419530eb17dc5a481335667a
Chanis Torres <wonderland@Chaniss-MacBook-Pro.local>
2020-06-04 23:59:05 -0400

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -13,6 +15,7 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
+          console.log("response", response);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -28,7 +31,15 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Route path ='/movie/:id'>
+        <Movie items = {movieList}/>
+      </Route>
+
+      <Route exact path ='/'>
+        <MovieList movies = {movieList}/>
+      </Route>
+     
     </div>
   );
 };
